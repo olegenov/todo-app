14e883dfa5ef3fa1c70ae3d4fb152efe018fbb70
olegenov <nik_kitaev@mail.ru>
2024-06-18 16:48:14 +0300

    Project frame added

---
+++ ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Никита Китаев on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
