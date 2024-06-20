cefef13a402431eb3b8eafa883283bf29557c3a4
olegenov <nik_kitaev@mail.ru>
2024-06-20 16:29:34 +0300

    extension TodoItem added

--- ToDoApp/ToDoApp/structs/TodoItem.swift
+++ ToDoApp/ToDoApp/structs/TodoItem.swift
@@ -20,11 +20,10 @@ struct TodoItem {
     let createdAt: Date
     let changedAt: Date?
     
-    init(id: String = UUID().uuidString, text: String, 
+    init(id: String = UUID().uuidString, text: String,
          importance: Importance, deadline: Date? = nil,
          isDone: Bool = false, createdAt: Date = Date(),
-         changedAt: Date? = nil
-        ) {
+         changedAt: Date? = nil) {
         self.id = id
         self.text = text
         self.importance = importance
@@ -32,5 +31,29 @@ struct TodoItem {
         self.isDone = isDone
         self.createdAt = createdAt
         self.changedAt = changedAt
+    }
+}
+
+extension TodoItem {
+    static func parse(json: Any) -> TodoItem? {
+        guard let data = json as? [String: Any], let id = data["id"] as? String,
+              let text = data["text"] as? String, let isDone = data["isDone"] as? Bool,
+              let createdAtTimestamp = data["createdAt"] as? TimeInterval else {
+            return nil
+        }
+        
+        let importanceRaw = data["importance"] as? String
+        let importance = Importance(rawValue: importanceRaw ?? Importance.medium.rawValue) ?? .medium
+        
+        let createdAt = Date(timeIntervalSince1970: createdAtTimestamp)
+        
+        let changedAtRaw = data["changedAt"] as? TimeInterval
+        let changedAt = changedAtRaw != nil ? Date(timeIntervalSince1970: changedAtRaw!) : nil
+        
+        let deadlineRaw = data["deadline"] as? TimeInterval
+        let deadline = deadlineRaw != nil ? Date(timeIntervalSince1970: deadlineRaw!) : nil
+        
+        return TodoItem(id: id, text: text, importance: importance, deadline: deadline,
+                        isDone: isDone, createdAt: createdAt, changedAt: changedAt)
     }
 }
