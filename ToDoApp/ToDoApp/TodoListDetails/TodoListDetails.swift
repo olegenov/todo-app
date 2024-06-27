69901542cb2236e210ca8849a9ba67940a090c67
olegenov <nik_kitaev@mail.ru>
2024-06-27 14:49:11 +0300

    Add button added

--- ToDoApp/ToDoApp/TodoListDetails/TodoListDetails.swift
+++ ToDoApp/ToDoApp/TodoListDetails/TodoListDetails.swift
@@ -9,7 +9,7 @@ struct TodoListDetails: View {
   @StateObject private var todoItemsList = TodoItemsList()
   @State private var showCompleted: Bool = false
   @Environment(\.colorScheme) var colorScheme
-
+  
   var addNewCell: some View {
     Text("Новое")
       .modifier(ListItemNewText())
@@ -35,6 +35,27 @@ struct TodoListDetails: View {
     }
   }
   
+  var addButton: some View {
+    Button(action: {
+      
+    }) {
+      ZStack {
+        Image(systemName: "circle.fill")
+          .renderingMode(.template)
+          .resizable()
+          .frame(width: 44, height: 44)
+          .foregroundColor(Color.white)
+        
+        Image(systemName: "plus.circle.fill")
+          .renderingMode(.template)
+          .resizable()
+          .frame(width: 44, height: 44)
+          .foregroundColor(Color.blue)
+      }
+      .shadow(color: Color(UIColor(red: 0/255, green: 73/255, blue: 153/255, alpha: 0.6)), radius: 20, x: 0, y: 8)
+    }
+  }
+  
   var todoList: some View {
     List {
       Section {
@@ -43,7 +64,7 @@ struct TodoListDetails: View {
             TodoItemRow(item: item)
               .listRowInsets(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
               .listRowBackground(Color.listRowBackground)
-
+            
           }
         }
         
@@ -75,9 +96,16 @@ struct TodoListDetails: View {
           .navigationTitle("Мои дела")
       }
       .background(Color.backgroundColor)
+      
+      .toolbar {
+        ToolbarItem(placement: .bottomBar) {
+          addButton
+        }
+      }
     }
     .padding(.horizontal, 16)
     .background(Color.backgroundColor)
+    
   }
   
   private func toggleComplited(for item: TodoItemModel) {
