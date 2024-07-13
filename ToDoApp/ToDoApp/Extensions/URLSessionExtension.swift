9f54e0b527970ea4f8469ffc8251061fcfedc888
olegenov <nik_kitaev@mail.ru>
2024-07-13 05:36:28 +0300

    kostil fixed

--- ToDoApp/ToDoApp/Extensions/URLSessionExtension.swift
+++ ToDoApp/ToDoApp/Extensions/URLSessionExtension.swift
@@ -9,14 +9,12 @@ extension URLSession {
   func dataTask(for urlRequest: URLRequest) async throws -> (Data, URLResponse) {
     try await withCheckedThrowingContinuation { continuation in
       let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-        if let error = error {
-          if error.localizedDescription == "cancelled" {
+        if let error = error as? NSError {
+          if error.code == NSURLErrorCancelled {
             continuation.resume(throwing: CancellationError())
-
-            return
+          } else {
+            continuation.resume(throwing: error)
           }
-
-          continuation.resume(throwing: error)
         } else if let data = data, let response = response {
           continuation.resume(returning: (data, response))
         } else {
